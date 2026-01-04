3aa33a38690258799224291317ac7696995cbaf9
Methuka Fernando <methuka.20242180@iit.ac.lk>
2026-01-04 12:40:56 +0530

    successfully carried out the first test

--- evernest/vite.config.js
+++ evernest/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    dedupe: ['react', 'react-dom'],
+    dedupe: ['react', 'react-dom', 'react-router-dom'],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
   },
   base: "/Adv.ClientSide-final-coursework/"
 })
